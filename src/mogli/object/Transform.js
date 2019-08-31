41c517590ce0e5913cc8c8e705ab795db76219f5
Andrew <andykuo1supergreen@gmail.com>
2019-08-31 15:14:32 -0700

    Added forward, up, and right vectors for transform

--- src/mogli/object/Transform.js
+++ src/mogli/object/Transform.js
@@ -1,5 +1,9 @@
 import { vec3, quat, mat4 } from 'gl-matrix';
 
+export const XAXIS = vec3.fromValues(1, 0, 0);
+export const YAXIS = vec3.fromValues(0, 1, 0);
+export const ZAXIS = vec3.fromValues(0, 0, 1);
+
 export function create()
 {
     return {
@@ -12,4 +16,33 @@ export function create()
 export function getTransformationMatrix(transform, dst = mat4.create())
 {
     return mat4.fromRotationTranslationScale(dst, transform.rotation, transform.translation, transform.scale);
+}
+
+// NOTE: From Mr.Dobbs himself...
+// https://www.gamedev.net/forums/topic/56471-extracting-direction-vectors-from-quaternion/
+export function getForwardVector(transform, dst = vec3.create())
+{
+    const q = transform.rotation;
+    dst[0] = 2 * (q[0] * q[2] + q[3] * q[1]);
+    dst[1] = 2 * (q[1] * q[2] + q[3] * q[0]);
+    dst[2] = 1 - 2 * (q[0] * q[0] + q[1] * q[1]);
+    return dst;
+}
+
+export function getUpVector(transform, dst = vec3.create())
+{
+    const q = transform.rotation;
+    dst[0] = 2 * (q[0] * q[1] + q[3] * q[2]);
+    dst[1] = 1 - 2 * (q[0] * q[0] + q[2] * q[2]);
+    dst[2] = 2 * (q[1] * q[2] + q[3] * q[0]);
+    return dst;
+}
+
+export function getRightVector(transform, dst = vec3.create())
+{
+    const q = transform.rotation;
+    dst[0] = 1 - 2 * (q[1] * q[1] + q[2] * q[2]);
+    dst[1] = 2 * (q[0] * q[1] + q[3] * q[2]);
+    dst[2] = 2 * (q[0] * q[2] + q[3] * q[1]);
+    return dst;
 }
