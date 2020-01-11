e9ad2f5de1f4c6eb59a307b4e28261b7468f8b88
Andrew Kuo <andykuo1supergreen@gmail.com>
2020-01-11 12:49:42 -0800

    Updated sweep to use new Milque libs

---
+++ packages/examples/01_sweep/2.0.0/MainRender.js
@@ -0,0 +1,77 @@
+import { Utils } from './milque.js';
+
+import * as MainScene from './MainScene.js';
+import * as Chunk from './Chunk.js';
+import * as PlayerControls from './PlayerControls.js';
+
+const HEALTH_X = 0;
+const HEALTH_Y = 0;
+const FRAMES_PER_SECOND = 60;
+
+export async function load()
+{
+
+}
+
+export function unload()
+{
+
+}
+
+export function onRender(view, world)
+{
+    let ctx = view.context;
+
+    Chunk.renderChunk(view, world.chunk);
+
+    let mouseX = PlayerControls.MOUSE_X.value * view.width;
+    let mouseY = PlayerControls.MOUSE_Y.value * view.height;
+    let mouseTileX = Utils.clampRange(Math.floor((mouseX - Chunk.CHUNK_OFFSET_X) / Chunk.TILE_SIZE), 0, Chunk.CHUNK_WIDTH - 1);
+    let mouseTileY = Utils.clampRange(Math.floor((mouseY - Chunk.CHUNK_OFFSET_Y) / Chunk.TILE_SIZE), 0, Chunk.CHUNK_HEIGHT - 1);
+    
+    Utils.drawBox(ctx,
+        Chunk.CHUNK_OFFSET_X + Chunk.TILE_OFFSET_X + mouseTileX * Chunk.TILE_SIZE,
+        Chunk.CHUNK_OFFSET_Y + Chunk.TILE_OFFSET_Y + mouseTileY * Chunk.TILE_SIZE,
+        0, Chunk.TILE_SIZE - 1, Chunk.TILE_SIZE - 1, 'rgba(0, 0, 0, 0.2)');
+    
+    // Draw shadow
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+    ctx.fillRect(0, 0, view.width, Chunk.CHUNK_OFFSET_Y);
+    ctx.fillRect(0, view.height - Chunk.CHUNK_OFFSET_Y, view.width, Chunk.CHUNK_OFFSET_Y);
+    ctx.fillRect(0, Chunk.CHUNK_OFFSET_Y, Chunk.CHUNK_OFFSET_X, view.height - Chunk.CHUNK_OFFSET_Y * 2);
+    ctx.fillRect(view.width - Chunk.CHUNK_OFFSET_X, Chunk.CHUNK_OFFSET_Y, Chunk.CHUNK_OFFSET_X, view.height - Chunk.CHUNK_OFFSET_Y * 2);
+    
+    // Draw health
+    for(let i = 0; i < MainScene.MAX_HEALTH; ++i)
+    {
+        let color = i < world.health ? 'salmon' : 'darkgray';
+        Utils.drawBox(ctx,
+            HEALTH_X + Chunk.TILE_SIZE / 2 + i * Chunk.TILE_SIZE,
+            HEALTH_Y + Chunk.TILE_SIZE / 2,
+            0,
+            Chunk.TILE_SIZE - 3,
+            Chunk.TILE_SIZE - 3,
+            color);
+    }
+
+    if (world.gameOver)
+    {
+        Utils.drawBox(ctx, view.width / 2, view.height / 2, 0, view.width * 0.7, 8, 'black');
+        Utils.drawText(ctx, 'Game Over', view.width / 2 + 1, view.height / 2 + 1, 0, 32, 'black');
+        Utils.drawText(ctx, 'Game Over', view.width / 2 - 1, view.height / 2 - 1, 0, 32, 'white');
+        
+        Utils.drawText(ctx, 'Click to continue', view.width / 2 + 1, view.height / 2 + 24 + 1, 0, 16, 'black');
+        Utils.drawText(ctx, 'Click to continue', view.width / 2 - 1, view.height / 2 + 24 - 1, 0, 16, 'white');
+    }
+    else if (world.gameWin)
+    {
+        Utils.drawBox(ctx, view.width / 2, view.height / 2, 0, view.width * 0.7, 8, 'black');
+        Utils.drawText(ctx, 'Success!', view.width / 2 + 1, view.height / 2 + 1, 0, 32, 'black');
+        Utils.drawText(ctx, 'Success!', view.width / 2 - 1, view.height / 2 - 1, 0, 32, 'gold');
+        
+        Utils.drawText(ctx, 'Click to continue', view.width / 2 + 1, view.height / 2 + 24 + 1, 0, 16, 'black');
+        Utils.drawText(ctx, 'Click to continue', view.width / 2 - 1, view.height / 2 + 24 - 1, 0, 16, 'white');
+    }
+
+    Utils.drawText(ctx, `Time: ${Math.floor(world.gameTime / FRAMES_PER_SECOND)}`, view.width / 2, view.height - Chunk.TILE_SIZE, 0, 16, 'white');
+}
---
+++ packages/examples/01_sweep/2.0.0/MainScene.js
@@ -0,0 +1,121 @@
+import { Utils } from './milque.js';
+
+import * as Chunk from './Chunk.js';
+import * as PlayerControls from './PlayerControls.js';
+import * as Views from './Views.js';
+
+export const MAX_HEALTH = 3;
+
+export function onStart()
+{
+    this.chunk = Chunk.createChunk();
+
+    this.health = MAX_HEALTH;
+    this.gameOver = false;
+    this.gameWin = false;
+    this.gameTime = 0;
+}
+
+export function onPreUpdate(dt)
+{
+
+}
+
+export function onUpdate(dt)
+{
+    // Check if restarting...
+    if (PlayerControls.RESTART_ACTION.value)
+    {
+        restart(this);
+        return;
+    }
+
+    // Do stuff...
+    if (this.gameOver || this.gameWin)
+    {
+        // Do nothing...
+        if (PlayerControls.ACTIVE_ACTION.value || PlayerControls.MARK_ACTION.value)
+        {
+            restart(this);
+            return;
+        }
+    }
+    else
+    {
+        this.gameTime += dt;
+
+        if (PlayerControls.ACTIVE_ACTION.value)
+        {
+            let mouseX = PlayerControls.MOUSE_X.value * Views.WORLD_VIEW.width;
+            let mouseY = PlayerControls.MOUSE_Y.value * Views.WORLD_VIEW.height;
+            let mouseTileX = Utils.clampRange(Math.floor((mouseX - Chunk.CHUNK_OFFSET_X) / Chunk.TILE_SIZE), 0, Chunk.CHUNK_WIDTH - 1);
+            let mouseTileY = Utils.clampRange(Math.floor((mouseY - Chunk.CHUNK_OFFSET_Y) / Chunk.TILE_SIZE), 0, Chunk.CHUNK_HEIGHT - 1);
+            let result = Chunk.digTiles(this.chunk, mouseTileX, mouseTileY);
+
+            if (!result)
+            {
+                dealDamage(this, 1);
+            }
+
+            if (checkWinCondition(this.chunk))
+            {
+                gameWin(this);
+            }
+        }
+
+        if (PlayerControls.MARK_ACTION.value)
+        {
+            let mouseX = PlayerControls.MOUSE_X.value * Views.WORLD_VIEW.width;
+            let mouseY = PlayerControls.MOUSE_Y.value * Views.WORLD_VIEW.height;
+            let mouseTileX = Utils.clampRange(Math.floor((mouseX - Chunk.CHUNK_OFFSET_X) / Chunk.TILE_SIZE), 0, Chunk.CHUNK_WIDTH - 1);
+            let mouseTileY = Utils.clampRange(Math.floor((mouseY - Chunk.CHUNK_OFFSET_Y) / Chunk.TILE_SIZE), 0, Chunk.CHUNK_HEIGHT - 1);
+            Chunk.markTile(this.chunk, mouseTileX, mouseTileY);
+        }
+    }
+}
+
+export function onRender(view, world)
+{
+
+}
+
+function dealDamage(scene, damage)
+{
+    scene.health -= damage;
+    if (scene.health <= 0)
+    {
+        gameOver(scene);
+    }
+}
+
+function restart(scene)
+{
+    Chunk.setupMap(scene.chunk);
+    scene.gameOver = false;
+    scene.gameWin = false;
+    scene.gameTime = 0;
+    scene.health = MAX_HEALTH;
+}
+
+function gameWin(scene)
+{
+    scene.gameWin = true;
+}
+
+function gameOver(scene)
+{
+    scene.gameOver = true;
+}
+
+function checkWinCondition(chunk)
+{
+    for(let i = 0; i < chunk.tiles.length; ++i)
+    {
+        if (chunk.solids[i] > 0 && chunk.tiles[i] <= 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
