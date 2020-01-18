a3a7995dc57149ca2f99acb17b55d7d28804d616
Andrew Kuo <andykuo1supergreen@gmail.com>
2020-01-17 19:29:17 -0800

    Moved createComponentFactory() into a module

--- packages/examples/0x_catrail/1.0.0/entity/index.js
+++ packages/examples/0x_catrail/1.0.0/entity/index.js
@@ -1,11 +1,13 @@
+import * as ComponentFactory from './component/ComponentFactory.js';
+export { ComponentFactory };
+
 export * from './Query.js';
 export * from './World.js';
 
-export * from './DefaultEntity.js';
-export * from './DefaultComponent.js';
-
-export * from './component/ComponentFactory.js';
 export * from './component/ComponentHelper.js';
 export * from './component/TagComponent.js';
 
 export * from './hybrid/EntityComponent.js';
+
+export * from './DefaultEntity.js';
+export * from './DefaultComponent.js';
