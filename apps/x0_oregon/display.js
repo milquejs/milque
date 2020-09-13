e3a5259afec8329364069b789e9d99ec454026b0
Andrew Kuo <andykuo1supergreen@gmail.com>
2020-09-12 19:54:58 -0700

    Removed unused demos

--- apps/x0_oregon/display.js
+++
@@ -1,7 +0,0 @@
-import { DisplayPort } from './lib.js';
-
-export const DISPLAY = new DisplayPort();
-DISPLAY.id = 'main';
-DISPLAY.mode = 'fit';
-DISPLAY.debug = true;
-document.body.appendChild(DISPLAY);
--- apps/x0_peermap/lib.js
+++
@@ -1,2 +0,0 @@
-export { Logger } from '../../packages/util/src/Logger.js';
-export { CanvasView, Camera2D } from '../../packages/view/src/index.js';
