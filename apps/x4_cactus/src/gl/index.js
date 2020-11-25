46f52be9c1cca9ffaf9d57e4d5248472b7782a1a
Andrew Kuo <andykuo1supergreen@gmail.com>
2020-11-25 00:02:41 -0800

    Added new type info helpers to index

--- apps/x4_cactus/src/gl/index.js
+++ apps/x4_cactus/src/gl/index.js
@@ -2,3 +2,6 @@ export * from './GLHelper.js';
 export * from './GLTypeInfo.js';
 export * from './ProgramHelper.js';
 export * from './ProgramInfo.js';
+export * from './UniformTypeInfo.js';
+export * from './UniformHelper.js';
+export * from './AttributeHelper.js';
