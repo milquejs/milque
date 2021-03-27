ca81fa44860e67a53b324aff7069a0dbba0c0502
Andrew Kuo <andykuo1supergreen@gmail.com>
2021-03-27 12:21:57 -0700

    Added player for gnomes

--- apps/x5_gnomes/src/main.js
+++ apps/x5_gnomes/src/main.js
@@ -7,6 +7,12 @@ import { TexturedQuadRenderer } from './renderer/TexturedQuadRenderer.js';
 import { ColoredQuadRenderer } from './renderer/ColoredQuadRenderer.js';
 import * as WebGLTextureLoader from './WebGLTextureLoader.js';
 
+/**
+ * @typedef {import('@milque/display').DisplayPort} DisplayPort
+ * @typedef {import('@milque/display').FrameEvent} FrameEvent
+ * @typedef {import('@milque/input').InputPort} InputPort
+ */
+
 window.addEventListener('error', error, true);
 window.addEventListener('unhandledrejection', error, true);
 function error(e)
@@ -28,30 +34,78 @@ function error(e)
 window.addEventListener('DOMContentLoaded', main);
 async function main()
 {
-    /** @type {import('@milque/display').DisplayPort}  */
+    /** @type {DisplayPort} */
     const display = document.querySelector('#display');
-    /** @type {import('@milque/input').InputPort}  */
+    /** @type {InputPort} */
     const input = document.querySelector('#input');
     input.src = {
-        MoveLeft: [ 'Keyboard:ArrowLeft', 'Keyboard:KeyA'],
+        MoveLeft: [ 'Keyboard:ArrowLeft', 'Keyboard:KeyA' ],
         MoveRight: [ 'Keyboard:ArrowRight', 'Keyboard:KeyD' ],
         MoveUp: [ 'Keyboard:ArrowUp', 'Keyboard:KeyW' ],
         MoveDown: [ 'Keyboard:ArrowDown', 'Keyboard:KeyS' ],
+        Interact: [ 'Keyboard:KeyE' ],
+        Evade: [ 'Keyboard:Space' ],
     };
 
+    /**
+     * @typedef Game
+     * @property {DisplayPort} display
+     * @property {number} frames
+     * @property {InputPort} input
+     * @property {object} world
+     */
+
+    /** @type {Game} */
+    const game = {
+        display,
+        frames: 0,
+        input,
+        world: {}
+    };
+
+    const updater = worldUpdater(game);
+    const renderer = worldRenderer(game);
+    
+    display.addEventListener('frame', e => {
+        const frameEvent = /** @type {FrameEvent} */(e);
+        const dt = frameEvent.detail.deltaTime / 60;
+
+        updater(dt);
+        renderer();
+    });
+}
+
+/**
+ * @param {Game} game 
+ * @returns {Function}
+ */
+function worldUpdater(game)
+{
+    const world = game.world;
+
+    let player = createPlayer(game);
+    world.player = player;
+    
+    return function(dt)
+    {
+        updatePlayer(game, player, dt);
+    };
+}
+
+/**
+ * @param {Game} game 
+ * @returns {Function}
+ */
+function worldRenderer(game)
+{
+    /** @type {DisplayPort} */
+    const display = game.display;
+    
     /** @type {WebGLRenderingContext} */
-    const gl = display.canvas.getContext('webgl');
-
-    const world = {
-        render: {
-            display,
-            frames: 0,
-            gl,
-        }
-    };
-
+    let gl = display.canvas.getContext('webgl');
     gl.clearColor(0, 0, 0, 0);
 
+    let camera = new OrthographicCamera(0, 0, 10, 10, 0, 10);
     let projectionViewMatrix = mat4.create();
 
     let texture = WebGLTextureLoader.load('color.png', { gl });
@@ -59,13 +113,11 @@ async function main()
 
     let texturedRenderer = new TexturedQuadRenderer(gl);
     let coloredRenderer = new ColoredQuadRenderer(gl);
-    let camera = new OrthographicCamera(0, 0, 10, 10, 0, 10);
     
-    display.addEventListener('frame', e => {
-        const { deltaTime } = e.detail;
-        const dt = deltaTime / 60;
-
-        world.render.frames += 1;
+    return function()
+    {
+        game.frames += 1;
+        
         let viewportWidth = gl.canvas.width;
         let viewportHeight = gl.canvas.height;
         gl.viewport(0, 0, viewportWidth, viewportHeight);
@@ -92,5 +144,58 @@ async function main()
             .setTexture(fontTexture, 64, 40)
             .setSprite(0, 0, 8, 8)
             .draw(1, 0, 1, 1);
-    });
+
+        texturedRenderer.setTexture(fontTexture, 64, 40);
+
+        // Draw player
+        renderPlayer(game, game.world.player, texturedRenderer, fontTexture);
+    };
+}
+
+/* == PLAYER ===================================================== */
+
+/**
+ * @param {Game} game
+ */
+function createPlayer(game)
+{
+    let player = {
+        x: 0, y: 0,
+        motionX: 0,
+        motionY: 0,
+        renderInfo: {
+            u: 56, v: 32,
+        }
+    };
+    return player;
+}
+
+/**
+ * @param {Game} game
+ */
+function updatePlayer(game, player, dt)
+{
+    let input = game.input;
+    let moveSpeed = 0.1;
+    let friction = 0.3;
+    let invFriction = 1 - friction;
+    let dx = input.getInputState('MoveRight') - input.getInputState('MoveLeft');
+    let dy = input.getInputState('MoveDown') - input.getInputState('MoveUp');
+    player.motionX += dx * moveSpeed * dt;
+    player.motionY += dy * moveSpeed * dt;
+    player.motionX *= invFriction;
+    player.motionY *= invFriction;
+    player.x += player.motionX;
+    player.y += player.motionY;
+}
+
+/**
+ * @param {Game} game
+ */
+function renderPlayer(game, player, texturedRenderer, fontTexture)
+{
+    texturedRenderer
+        .setTexture(fontTexture, 64, 40)
+        .setSprite(player.renderInfo.u, player.renderInfo.v, 8, 8)
+        .draw(player.x, player.y);
 }
