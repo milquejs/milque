392fe7374ddfeb3c1f719b57deb67f224a92303c
Andrew Kuo <andykuo1supergreen@gmail.com>
2021-03-28 22:44:16 -0700

    Added AssetManager to gnomes

--- apps/x5_gnomes/README.md
+++ apps/x5_gnomes/README.md
@@ -1,3 +1,45 @@
 # Gnomes
 
 A game of gnomes.
+
+An RPG where you are a collection of gnomes. Do a quest.
+
+
+
+
+---
+
+A detective game in the style of Wes Anderson. 
+
+We are telling a murder mystery, who is being written by the writer.
+
+It gives hard facts, but you can never shake off the feeling that SOMEONE is telling you all this (me). There's a right and wrong, but it's a skewed persepctive.
+
+
+Mechanics:
+
+Case of the Red Pelican.
+
+Someone has stolen a Red Jewel.
+
+Three suspects were at the scene.
+
+A chef.
+
+A art gallery person.
+
+A security guard.
+
+
+Build the schedule of each person?
+- This is by traits, profession, habits, personality, life circumstance, etc.
+
+- 1. MURDER SCENE
+    - Someone is dead. And suspects are gathered.
+- 2. Interrogation
+    - People explain their whereabouts and themselves.
+    - Clues are identified.
+- 3. Red Herring / Twist
+    - 
+- 4. Reveal
+- 5. Rationale / Epilogue
---
+++ apps/x5_gnomes/src/assets/Audio.js
@@ -0,0 +1,104 @@
+export const AUDIO_CONTEXT = new AudioContext();
+autoUnlock(AUDIO_CONTEXT);
+
+export async function loadAudio(filepath, opts = {})
+{
+    const ctx = AUDIO_CONTEXT;
+
+    let result = await fetch(filepath);
+    let buffer = await result.arrayBuffer();
+    let data = await ctx.decodeAudioData(buffer);
+    return new Sound(ctx, data, Boolean(opts.loop));
+}
+
+const DEFAULT_SOURCE_PARAMS = {
+    gain: 0,
+    pitch: 0,
+    pan: 0,
+    loop: false,
+};
+class Sound
+{
+    constructor(ctx, audioBuffer, loop = false)
+    {
+        this.context = ctx;
+        this.buffer = audioBuffer;
+
+        this._source = null;
+
+        this.playing = false;
+        this.loop = loop;
+
+        this.onAudioSourceEnded = this.onAudioSourceEnded.bind(this);
+    }
+
+    onAudioSourceEnded()
+    {
+        this._playing = false;
+    }
+
+    play(opts = DEFAULT_SOURCE_PARAMS)
+    {
+        if (!this.buffer) return;
+        if (this._source) this.destroy();
+
+        const ctx = this.context;
+        let source = ctx.createBufferSource();
+        source.addEventListener('ended', this.onAudioSourceEnded);
+        source.buffer = this.buffer;
+        source.loop = opts.loop;
+
+        let prevNode = source;
+
+        // https://www.oreilly.com/library/view/web-audio-api/9781449332679/ch04.html
+        // Add pitch
+        if (opts.pitch)
+        {
+            source.detune.value = opts.pitch * 100;
+        }
+
+        // Add gain
+        if (opts.gain)
+        {
+            const gainNode = ctx.createGain();
+            gainNode.gain.value = opts.gain;
+            prevNode = prevNode.connect(gainNode);
+        }
+
+        // Add stereo pan
+        if (opts.pan)
+        {
+            const pannerNode = ctx.createStereoPanner();
+            pannerNode.pan.value = opts.pan;
+            prevNode = prevNode.connect(pannerNode);
+        }
+
+        prevNode.connect(ctx.destination);
+        source.start();
+
+        this._source = source;
+        this._playing = true;
+    }
+
+    pause()
+    {
+        this._source.stop();
+        this._playing = false;
+    }
+
+    destroy()
+    {
+        if (this._source) this._source.disconnect();
+        this._source = null;
+    }
+}
+
+async function autoUnlock(ctx)
+{
+    const callback = () => {
+        if (ctx.state === 'suspended') {
+            ctx.resume();
+        }
+    };
+    document.addEventListener('click', callback);
+}
