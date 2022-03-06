bef7c1026766fbaa92f8a27f59d5cc4cca2bdbb7
andykuo1 <andykuo1supergreen@gmail.com>
2022-03-05 21:01:34 -0800

    Add text renderer to sandbox

---
+++ apps/x0_sandbox/src/loader/TextLoader.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} src 
+ * @returns {string}
+ */
+export async function loadText(src)
+{
+    if (typeof src === 'string')
+    {
+        const response = await fetch(src);
+        const arrayBuffer = await response.arrayBuffer();
+        return loadText(arrayBuffer);
+    }
+    else if (!(src instanceof ArrayBuffer || ArrayBuffer.isView(src)))
+    {
+        throw new Error(
+            'Cannot load from source - must be '
+            + 'an array buffer or fetchable url');
+    }
+    /** @type {ArrayBuffer} */
+    const arrayBuffer = src;
+    return new TextDecoder().decode(arrayBuffer);
+}
