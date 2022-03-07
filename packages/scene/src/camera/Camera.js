873b1b3f1515e8fc08ebe9a5ac6dce6a46b689a3
andykuo1 <andykuo1supergreen@gmail.com>
2022-03-07 08:13:44 -0800

    Add prettier to milque/scene

--- packages/scene/src/camera/Camera.js
+++ packages/scene/src/camera/Camera.js
@@ -1,20 +1,17 @@
-export class Camera
-{
-    constructor(projectionMatrix, viewMatrix)
-    {
-        this.projectionMatrix = projectionMatrix;
-        this.viewMatrix = viewMatrix;
-    }
+export class Camera {
+  constructor(projectionMatrix, viewMatrix) {
+    this.projectionMatrix = projectionMatrix;
+    this.viewMatrix = viewMatrix;
+  }
 
-    /**
-     * @abstract
-     * @param {number} [viewportWidth]
-     * @param {number} [viewportHeight]
-     * @returns {Camera}
-     */
-    // eslint-disable-next-line no-unused-vars
-    resize(viewportWidth = undefined, viewportHeight = undefined)
-    {
-        return this;
-    }
+  /**
+   * @abstract
+   * @param {number} [viewportWidth]
+   * @param {number} [viewportHeight]
+   * @returns {Camera}
+   */
+  // eslint-disable-next-line no-unused-vars
+  resize(viewportWidth = undefined, viewportHeight = undefined) {
+    return this;
+  }
 }
--- packages/scene/src/camera/PerspectiveCamera.js
+++ packages/scene/src/camera/PerspectiveCamera.js
@@ -3,25 +3,29 @@ import { Camera } from './Camera.js';
 
 const DEFAULT_FOVY = Math.PI / 3;
 
-export class PerspectiveCamera extends Camera
-{
-    constructor(fieldOfView = DEFAULT_FOVY, near = 0.1, far = 1000)
-    {
-        super(mat4.create(), mat4.create());
+export class PerspectiveCamera extends Camera {
+  constructor(fieldOfView = DEFAULT_FOVY, near = 0.1, far = 1000) {
+    super(mat4.create(), mat4.create());
 
-        this.fieldOfView = Number(fieldOfView);
-        this.clippingPlane = {
-            near: Number(near),
-            far: Number(far),
-        };
-    }
+    this.fieldOfView = Number(fieldOfView);
+    this.clippingPlane = {
+      near: Number(near),
+      far: Number(far),
+    };
+  }
 
-    /** @override */
-    resize(viewportWidth = undefined, viewportHeight = undefined)
-    {
-        const aspectRatio = typeof viewportWidth === 'undefined' ? 1 : viewportWidth / viewportHeight;
-        const { near, far } = this.clippingPlane;
-        mat4.perspective(this.projectionMatrix, this.fieldOfView, aspectRatio, near, far);
-        return this;
-    }
+  /** @override */
+  resize(viewportWidth = undefined, viewportHeight = undefined) {
+    const aspectRatio =
+      typeof viewportWidth === 'undefined' ? 1 : viewportWidth / viewportHeight;
+    const { near, far } = this.clippingPlane;
+    mat4.perspective(
+      this.projectionMatrix,
+      this.fieldOfView,
+      aspectRatio,
+      near,
+      far
+    );
+    return this;
+  }
 }
